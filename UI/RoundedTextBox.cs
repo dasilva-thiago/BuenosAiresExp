9b56b94cf341e79f0b9c9dfd67c1f862305414ea
THIAGO DA SILVA <thiagosilva785@gmail.com>
2026-03-15 16:14:05 -0300

    update UI - roundedtextbox

--- UI/RoundedTextBox.cs
+++ UI/RoundedTextBox.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace BuenosAiresExp.UI
 {
-    internal class RoundedTextBox
+    internal class RoundedTextBox : UserControl
     {
+        private TextBox _textBox;
+        private int _borderRadius = 6;
+        private Color _borderColor = BuenosAiresTheme.BorderColor;
+        private Color _focusColor = BuenosAiresTheme.PrimaryColor;
+        private bool _isFocused = false; // Variável para controlar o estado de foco
+
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public string Value
+        {
+            get => _textBox.Text;
+            set => _textBox.Text = value;
+
+        }
+
+        public bool IsPassword
+        {
+            set => _textBox.UseSystemPasswordChar = value;
+        }
+
     }
 }
