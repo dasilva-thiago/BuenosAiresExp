50930aeded2ba88710157cb8af509c5e9e150e88
THIAGO DA SILVA <thiagosilva785@gmail.com>
2026-03-14 21:08:02 -0300

    Add UI - font, colors

---
+++ Forms/LocationForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BuenosAiresExp
+{
+    public partial class LocationForm : Form
+    {
+        public LocationForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
